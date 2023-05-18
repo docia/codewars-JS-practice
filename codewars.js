d084807636fddb16a32316303e70eaa84c3e589c
docia <102433497+docia@users.noreply.github.com>
2023-05-18 12:58:59 +0300

    codewars tasks resolved

--- codewars.js
+++ codewars.js
@@ -1,1 +1,29 @@
+// https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
 
+const goals = (laLigaGoals, copaDelReyGoals, championsLeagueGoals) => laLigaGoals + copaDelReyGoals + championsLeagueGoals
+
+// https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+
+function makeNegative(num) {
+  if (num <= 0) return num;
+  return num * -1;
+}
+
+// https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
+
+const move = (position, roll) => roll * 2 + position
+
+//https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
+
+const greet = (name, owner) => (owner == name ? "Hello boss" : "Hello guest");
+
+// https://www.codewars.com/kata/keep-hydrated-1/train/javascript
+
+const litres = time => Math.floor(time * 0.5) 
+
+
+// https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
+
+function lovefunc(flower1, flower2) {
+  return flower1 % 2 != flower2 % 2
+}
