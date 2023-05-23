75eb06b357a0930439cc2959512ba682a33f8621
docia <102433497+docia@users.noreply.github.com>
2023-05-23 22:38:56 +0300

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -1,1 +1,32 @@
+//https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
 
+function stringToArray(string) {
+  for (let i = 0; i <= string.length; i++) {
+    return string.split(" ");
+  }
+}
+
+//https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+
+function DNAtoRNA(dna) {
+  return dna.replace(/T/g, "U")
+}
+//https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
+
+var min = function (list) {
+  return Math.min(...list);
+};
+
+var max = function (list) {
+  return Math.max(...list);
+};
+
+//https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
+
+function min(arr, toReturn) {
+  if (toReturn === "value") {
+    return Math.min(...arr);
+  } else {
+    return arr.indexOf(Math.min(...arr));
+  }
+}
