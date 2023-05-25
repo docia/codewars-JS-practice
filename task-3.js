955829eb33210d428edbabb313d540259c5cbca6
docia <102433497+docia@users.noreply.github.com>
2023-05-25 13:40:43 +0300

    Update task-3.js

--- task-3.js
+++ task-3.js
@@ -8,6 +8,9 @@ class SmallestIntegerFinder {
 
 //https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
 
+function circleCircumference(circle) {
+  return 2 * Math.PI * circle.radius
+}
 
 //https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
 
